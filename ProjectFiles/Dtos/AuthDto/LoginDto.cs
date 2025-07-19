5e973b1ffe170bae112a226118356f9f45e5b83d
KeYRun DHKL <keyrundhkl@gmail.com>
2025-07-19 17:08:25 +0545

    updated

--- ProjectFiles/Dtos/AuthDto/LoginDto.cs
+++ ProjectFiles/Dtos/AuthDto/LoginDto.cs
@@ -7,7 +7,7 @@ public class LoginDto
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Invalid email address format.")]
     public required string Email { get; set; }
-
+    
     [Required(ErrorMessage = "Password is required.")]
     [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 100 characters.")]
     [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{6,}$", 
