8495704532a20bb82adca987326c5ed2475e4ef7
KeYRun DHKL <keyrundhkl@gmail.com>
2025-07-19 17:08:25 +0545

    updated

--- ProjectFiles/Dtos/AuthDto/AuthResponseDto.cs
+++ ProjectFiles/Dtos/AuthDto/AuthResponseDto.cs
@@ -1,12 +1,8 @@
 namespace AgroTechProject.Dtos.AuthDto;
-
 public class AuthResponseDto
 {
     public required string Token { get; set; } = null!;
-
     public required string RefreshToken { get; set; } = null!;
     public required string Role { get; set; } = null!;
     public required string FullName { get; set; } = null!;
-
-
 }
